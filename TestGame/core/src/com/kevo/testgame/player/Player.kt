0acd7abddfbb2a284a0e2ba95bbf8e4154a887e0
Kevin Schanz <kevinschanz@live.com>
2021-05-27 17:58:06 -0400

    [TestGame] Add initial player physics framework

--- TestGame/core/src/com/kevo/testgame/player/Player.kt
+++ TestGame/core/src/com/kevo/testgame/player/Player.kt
@@ -2,6 +2,12 @@ package com.kevo.testgame.player
 
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.physics.box2d.BodyDef
+import com.badlogic.gdx.physics.box2d.Fixture
+import com.badlogic.gdx.physics.box2d.FixtureDef
+import com.badlogic.gdx.physics.box2d.PolygonShape
+import com.badlogic.gdx.physics.box2d.World
 
 /**
  * Represents the *Player* character. The new Player instance will be created at
@@ -10,17 +16,69 @@ import com.badlogic.gdx.graphics.g2d.Sprite
  *
  * @constructor Creates a new Player instance at the specified location.
  *
+ * @property world The world in which the `Player` exists.
+ *
  * @param textureFileName The name of the texture file, ex: "Player.png".
  * @param x The X-coordinate where the `Player` should be placed.
  * @param y The Y-coordinate where the `Player` should be placed.
  */
 class Player(
+        /**
+         * Create the world in the scene where we need physics. For example, we
+         * don't need physics in the main menu, or the options menu.
+         */
+        private val world: World,
         textureFileName: String,
         x: Float,
         y: Float
 ) : Sprite(Texture(textureFileName)) {
 
+    /**
+     * The actual body of the player. We will want to move the body and manipulate
+     * the player. The body can have shape, friction, mass, gravity, rotation, and
+     * so on.
+     */
+    private var body: Body? = null
+
     init {
         setPosition((x - width) / 2, (y - height) / 2)
+        createPhysicsBody()
+    }
+
+    private fun createPhysicsBody() {
+        // Define whether the body is dynamic, static, or kinematic.
+        val bodyDef = BodyDef()
+
+        /*
+        Body Definition Types:
+        - A static body is not affected by gravity or other forces.
+        - A kinematic body is not affected by gravity, but is affected by other forces.
+        - A dynamic body is affected by gravity and other forces.
+         */
+        bodyDef.type = BodyDef.BodyType.DynamicBody
+        bodyDef.position.set(this.x, this.y)
+
+        body = world.createBody(bodyDef)
+
+        body?.let {
+            // Shape of the physics body.
+            val shape = PolygonShape()
+            shape.setAsBox(this.width / 2, this.height / 2)
+
+            val fixtureDef = FixtureDef()
+            fixtureDef.shape = shape
+            fixtureDef.density = 1f
+
+            val fixture: Fixture = it.createFixture(fixtureDef)
+
+            // Free up memory (system resources).
+            shape.dispose()
+        }
+    }
+
+    fun updatePlayer() {
+        body?.let {
+            setPosition(it.position.x, it.position.y)
+        }
     }
 }
--- TestGame/core/src/com/kevo/testgame/scenes/MainMenu.kt
+++ TestGame/core/src/com/kevo/testgame/scenes/MainMenu.kt
@@ -2,8 +2,11 @@ package com.kevo.testgame.scenes
 
 import com.badlogic.gdx.ApplicationListener
 import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.World
 import com.badlogic.gdx.utils.ScreenUtils
 import com.kevo.testgame.GameMain
 import com.kevo.testgame.helpers.GameInfo
@@ -12,10 +15,19 @@ import com.kevo.testgame.player.Player
 class MainMenu(private val game: GameMain) : Screen {
 
     private val background = Texture("Game BG.png")
+
+    /**
+     * Create a world in which physics bodies are allowed to sleep (this
+     * makes the game engine more efficient, so calculations do not need
+     * to be performed for sleeping bodies.
+     */
+    private val world: World = World(Vector2(0f, -9.8f), true)
+
     private val player = Player(
-            "Player 1.png",
-            GameInfo.WIDTH.toFloat(),
-            GameInfo.HEIGHT.toFloat())
+            world = world,
+            textureFileName = "Player 1.png",
+            x = GameInfo.WIDTH.toFloat(),
+            y = GameInfo.HEIGHT.toFloat())
 
     init {
         /*
@@ -43,18 +55,21 @@ class MainMenu(private val game: GameMain) : Screen {
      * @param delta The time in seconds since the last render.
      */
     override fun render(delta: Float) {
+        player.updatePlayer()
+
         ScreenUtils.clear(1f, 0f, 0f, 1f)
 
         game.batch.begin()
         // The coordinate system in libGDX starts at the bottom-left corner (0, 0).
         game.batch.draw(background, 0f, 0f)
 
-        // Nudge the player sprite on each frame.
-        player.setPosition(player.x - 1, player.y)
-
         // Draw the Player sprite in the center of the world.
         game.batch.draw(player, player.x, player.y)
         game.batch.end()
+
+        // Time step, velocity iterations, and position iterations. Higher numbers
+        // result in better precision, but worse performance.
+        world.step(Gdx.graphics.deltaTime, 6, 2)
     }
 
     /**
@@ -90,5 +105,6 @@ class MainMenu(private val game: GameMain) : Screen {
      */
     override fun dispose() {
         background.dispose()
+        player.texture.dispose()
     }
 }
