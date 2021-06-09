39f292a1fcf42fa948abf9925eb37b3c7a107eee
Kevin Schanz <kevinschanz@live.com>
2021-06-09 17:20:32 -0400

    Add infinite scrolling background using orthographic main camera

--- JackTheGiantGame/core/src/com/kevo/jackgiant/scenes/Gameplay.kt
+++ JackTheGiantGame/core/src/com/kevo/jackgiant/scenes/Gameplay.kt
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
@@ -12,14 +13,18 @@ import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
 import com.badlogic.gdx.physics.box2d.World
 import com.badlogic.gdx.utils.ScreenUtils
+import com.badlogic.gdx.utils.viewport.StretchViewport
+import com.badlogic.gdx.utils.viewport.Viewport
 import com.kevo.jackgiant.GameInfo
 import com.kevo.jackgiant.GameMain
 import com.kevo.jackgiant.clouds.Cloud
 import com.kevo.jackgiant.player.Player
+import kotlin.math.abs
 
 class Gameplay(private val game: GameMain) : Screen {
 
     private var backgrounds: ArrayList<Sprite> = arrayListOf()
+    private var backgroundLastYPosition: Float = 0f
 
     /**
      * Create a world in which physics bodies are allowed to sleep (this
@@ -42,6 +47,25 @@ class Gameplay(private val game: GameMain) : Screen {
             textureFileName = "Clouds/Cloud 1.png")
 
     /**
+     * The 2D orthographic main game camera that works with the viewport to
+     * show the correct world contents to the user.
+     */
+    private val mainCamera: OrthographicCamera = OrthographicCamera(
+            GameInfo.WIDTH.toFloat(),
+            GameInfo.HEIGHT.toFloat())
+
+    /**
+     * Manages a [Camera] and determines how world coordinates are mapped
+     * to and from the screen. This is a scaling viewport that uses the
+     * `Stretch` scaling so it does not keep the aspect ratio, and the world
+     * is scaled to take the whole screen.
+     */
+    private val gameViewport: Viewport = StretchViewport(
+            GameInfo.WIDTH.toFloat(),
+            GameInfo.HEIGHT.toFloat(),
+            mainCamera)
+
+    /**
      * A 2D orthographic camera instance. Used to render outlines of the
      * physics bodies.
      */
@@ -49,12 +73,16 @@ class Gameplay(private val game: GameMain) : Screen {
 
     /**
      * Responsible for rendering the contents of the orthographic camera
-     * to the screen.
+     * to the screen. Can be used to render outlines of the physics bodies.
      */
     private val debugRenderer: Box2DDebugRenderer = Box2DDebugRenderer()
 
     init {
         createBackgrounds()
+
+        mainCamera.position.set(
+                GameInfo.WIDTH / 2f,
+                GameInfo.HEIGHT / 2f, 0f)
 
         box2DCamera.setToOrtho(false,
                 GameInfo.WIDTH.toFloat() / GameInfo.PPM,
@@ -74,6 +102,7 @@ class Gameplay(private val game: GameMain) : Screen {
             // Position each background below the last background (in a parallax
             // scrolling fashion).
             background.setPosition(0f, -(i * background.height))
+            backgroundLastYPosition = abs(background.y)
             backgrounds.add(background)
         }
     }
@@ -83,7 +112,8 @@ class Gameplay(private val game: GameMain) : Screen {
     }
 
     /**
-     * Update game world using [dt] "Delta Time".
+     * Update game world using [dt] "Delta Time" (the time between each frame,
+     * usually a very small number).
      */
     private fun update(dt: Float) {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
@@ -94,6 +124,47 @@ class Gameplay(private val game: GameMain) : Screen {
         } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             player.body.let {
                 it.applyForce(Vector2(2f, 0f), it.worldCenter, true)
+            }
+        }
+
+        moveCamera()
+        checkBackgroundsOutOfBounds()
+    }
+
+    private fun moveCamera() {
+        // TODO: The two cameras can probably be combined, may need to call mainCamera.setToOrtho.
+        mainCamera.position.y -= 1
+    }
+
+    private fun checkBackgroundsOutOfBounds() {
+        for (bg in backgrounds) {
+            /*
+            Check whether the background has scrolled up and out of the view of
+            the camera. If it's no longer visible, move it to the bottom the stack:
+
+            +------+
+            |      |
+            |  BG  |
+            |      |
+            +------+
+            +------+
+            |      |
+            |  BG  |
+            |      |
+            +------+
+            +------+
+            |      |
+            |  BG  |
+            |      |
+            +------+
+
+            We need to add some extra 5 pixel padding to keep up with the movement
+            speed of the camera, to achieve the "Infinite Scrolling Background" effect.
+             */
+            if (bg.y - (bg.height / 2f) - 5f > mainCamera.position.y) {
+                val newPosition = bg.height + backgroundLastYPosition
+                bg.setPosition(0f, -newPosition)
+                backgroundLastYPosition = abs(newPosition)
             }
         }
     }
@@ -132,7 +203,11 @@ class Gameplay(private val game: GameMain) : Screen {
 
         game.batch.end()
 
-        debugRenderer.render(world, box2DCamera.combined)
+        // Specify what we "see" with our camera.
+        game.batch.projectionMatrix = mainCamera.combined
+        mainCamera.update()
+
+        // debugRenderer.render(world, box2DCamera.combined)
 
         // Time step, velocity iterations, and position iterations. Higher numbers
         // result in better precision, but worse performance.
